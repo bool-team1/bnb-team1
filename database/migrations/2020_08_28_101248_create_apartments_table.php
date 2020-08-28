27e303677a777cde46f6fbd9b7db02fbaff03bc8
Bartolomeo Piotti <piotti.bartolomeo@gmail.com>
2020-08-28 14:19:18 +0200

    bozza migrations

---
+++ database/migrations/2020_08_28_101248_create_apartments_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateApartmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('apartments', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('user_id');
+            //Set user_id as foreign key
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->string('title', 30);
+            $table->string('address', 100);
+            $table->tinyInteger('rooms_n');
+            $table->tinyInteger('bathrooms_n');
+            $table->float('square_mt');
+            $table->unsignedDecimal('longitude', 9, 6);
+            $table->unsignedDecimal('latitude', 8, 6);
+            $table->string('slug', 50);
+            $table->boolean('isPublic');
+            $table->string('main_pic');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('apartments');
+    }
+}
---
+++ database/migrations/2020_08_28_114951_create_apartment_facility_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateApartmentFacilityTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('apartment_facility', function (Blueprint $table) {
+            $table->unsignedBigInteger('apartment_id');
+            //Set apartment_id as foreign key
+            $table->foreign('apartment_id')->references('id')->on('apartments');
+
+            $table->unsignedBigInteger('facility_id');
+            //Set facility_id as foreign key
+            $table->foreign('facility_id')->references('id')->on('facilities');
+
+            $table->primary(['apartment_id', 'facility_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('apartment_facility');
+    }
+}
