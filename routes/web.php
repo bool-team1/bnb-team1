5d504c94be0088288864d198c43cdc41a890c110
Veronica Celseti <veronicacelseti12@gmail.com>
2020-08-31 16:30:58 +0200

    Admin\ApartmentController+routes

--- routes/web.php
+++ routes/web.php
@@ -23,5 +23,5 @@ Route::get('/', 'HomeController@index')->name('home');
 Route::prefix('admin')->namespace('Admin')->name('admin.')->middleware('auth')->group(function() {
 //queste rotte iniziano con admin , sono le pagine della dashboard, navigabili solo con l'autenticazione
     Route::get('/', 'HomeController@index')->name('home');
-    
+    Route::resource('/apartment', 'ApartmentController');
 });
