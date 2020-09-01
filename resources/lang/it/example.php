3c2ef6d76ec4824785529db4db254e1427452a40
Daniele Nicolini <dnicolini.fusiorari@gmail.com>
2020-09-01 12:43:07 +0200

    Added translation example

---
+++ resources/lang/it/example.php
@@ -0,0 +1,11 @@
+<?php 
+
+return [
+    //Esempio di come costruire file di traduzione
+    //Traduzione semplice:
+    //'messaggio' -> 'testo del messaggio'
+    //Traduzione con attributo variabile, come il nome dell'utente:
+    //'messaggioconattributo' -> 'testo del messaggio :nome_attributo'
+    //Traduzione "variabile" a seconda dell'attributo, per esempio per modificare in caso di variabile singolare o plurale:
+    //'messaggio' -> '{1}Tu hai :attr_anni anno di età|[2,*]Tu hai :attr_anni anni di età'
+];
