21d7653b9af22ce0e6c334a211362eb5d4bacc10
Daniele Nicolini <dnicolini.fusiorari@gmail.com>
2020-09-01 16:26:40 +0200

    +Translation: Auto stash before rebase of "bnb-team1/master"

---
+++ database/migrations/2020_09_01_134930_create_ad_plan_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdPlanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ad_plan', function (Blueprint $table) {
+            $table->unsignedBigInteger('ad_id');
+            //Set apartment_id as foreign key
+            $table->foreign('ad_id')->references('id')->on('ads');
+            $table->unsignedBigInteger('plan_id');
+            //Set plan_id as foreign key
+            $table->foreign('plan_id')->references('id')->on('plans');
+            $table->primary(['ad_id', 'plan_id']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ad_plan');
+    }
+}
