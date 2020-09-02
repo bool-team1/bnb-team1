ee98c2f82eadba6bc6edff8480d43b7b5606e1c4
George <quarelli.george@gmail.com>
2020-09-02 12:43:51 +0200

    home view minor fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,15 +1,15 @@
-    {{-- menu + HEAD  --}}
-    @include("partials.menu")
-    {{-- fine menu --}}
-    <body>
+{{-- menu + HEAD  --}}
+@include("partials.menu")
+{{-- fine menu --}}
+<body>
 
-        {{-- main --}}
-        @yield('content')
-        {{-- fine main --}}
+    {{-- main --}}
+    @yield('content')
+    {{-- fine main --}}
 
-        {{-- footer --}}
-        @include('partials.footer')
-        {{-- fine footer --}}
-        <script src="{{("js/app.js")}}" charset="utf-8"></script>
-    </body>
+    {{-- footer --}}
+    @include('partials.footer')
+    {{-- fine footer --}}
+    <script src="{{("js/app.js")}}" charset="utf-8"></script>
+</body>
 </html>
