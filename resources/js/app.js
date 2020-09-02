2e4678cf8d4a5b5c454f5e065cfeaffeb4cd36cf
Luca Santini <lucasantinidev@gmail.com>
2020-09-02 19:00:21 +0200

    Installazione Algolia via npm

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,3 +2,18 @@ require('./bootstrap');
 
 //Dipendenza Jquery
 var $ = require( "jquery" );
+
+// Initialize the places.js library (Algolia)
+var places = require('places.js');
+
+var placesAutocomplete = places({
+  appId: 'plNNICPWC6MP',
+  apiKey: 'b7a397b2d5106c810a38e7f10cdd967a',
+  container: '#address-input';
+});
+
+$(document).ready(function(){
+    $('#addressInput').keypress(function(){
+        alert('Hello');
+    });
+});
