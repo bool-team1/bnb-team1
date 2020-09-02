5753e3ae4f5e686736a2d57c75171cd45835fd32
Bartolomeo Piotti <piotti.bartolomeo@gmail.com>
2020-09-02 17:16:34 +0200

    admin home almost responsive, footer temporary changes for admin layout

--- resources/views/layouts/app_admin.blade.php
+++ resources/views/layouts/app_admin.blade.php
@@ -4,13 +4,13 @@
 <body>
     <div class="container main-ctn">
             <div class="row">
-                <div class="sidebar col-2">
+                <div class="sidebar">
                     <div class="container">
                         <div class="row">
-                            <div class="col-md-12">
+                            <div class="col-md-12 side_wrap">
                                 <div class="user_info">
                                     <div class="prof_pic_wrapper">
-                                        {{-- <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" alt="Profile Picture"> --}}
+                                        <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" alt="Profile Picture">
                                     </div>
                                     <p>USER NAME</p>
                                 </div>
@@ -22,25 +22,25 @@
                                         </a>
                                     </li>
                                     <li>
-                                        <i class="far fa-envelope"></i>
+                                        <i class="fas fa-book-open"></i>
                                         <a href="#">
                                             PRENOTAZIONI
                                         </a>
                                     </li>
                                     <li>
-                                        <i class="fas fa-chart-pie"></i>
+                                        <i class="far fa-chart-bar"></i>
                                         <a href="#">
                                             STATISTICHE
                                         </a>
                                     </li>
                                     <li>
-                                        <i class="fas fa-chart-pie"></i>
+                                        <i class="far fa-envelope"></i>
                                         <a href="#">
                                             MESSAGGI
                                         </a>
                                     </li>
                                     <li>
-                                        <i class="fas fa-chart-pie"></i>
+                                        <i class="fas fa-sign-out-alt"></i>
                                         <a href="#">
                                             LOG OUT
                                         </a>
