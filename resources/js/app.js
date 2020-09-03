bce7aa7620317e7e85f413c4646b752aad50ceb4
Luca Santini <lucasantinidev@gmail.com>
2020-09-03 10:25:08 +0200

    Installazione libreria Algolia

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,17 +3,13 @@ require('./bootstrap');
 //Dipendenza Jquery
 var $ = require( "jquery" );
 
-// Initialize the places.js library (Algolia)
-var places = require('places.js');
-
-var placesAutocomplete = places({
-  appId: 'plNNICPWC6MP',
-  apiKey: 'b7a397b2d5106c810a38e7f10cdd967a',
-  container: '#address-input';
-});
-
 $(document).ready(function(){
-    $('#addressInput').keypress(function(){
-        alert('Hello');
+    var places = require('places.js');
+
+    var placesAutocomplete = places({
+      appId: 'plNNICPWC6MP',
+      apiKey: 'b7a397b2d5106c810a38e7f10cdd967a',
+      container: document.querySelector('#address-input')
     });
+
 });
