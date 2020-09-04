46fb9b140599fe1555d88968589d4ed435140b80
Daniele Nicolini <dnicolini.fusiorari@gmail.com>
2020-09-04 15:28:14 +0200

    Updated Registration with Css, translation and client-validation

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,20 @@
 require('./bootstrap');
 
-//Dipendenza Jquery
+//Jquery dependency
 var $ = require( "jquery" );
+
+$(document).ready(function(){
+    
+    //CLIENT SIDE VALIDATION
+    //Validation for registration form email
+    var registration_form_email = document.getElementById("email");
+
+    registration_form_email.addEventListener("input", function (event) {
+        if (email.validity.typeMismatch) {
+          email.setCustomValidity("Inserisci un indirizzo email valido");
+        } else {
+          email.setCustomValidity("");
+        }
+    });
+
+});
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -4,10 +4,10 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+            <div class="card custom-auth-form">
+                <div class="card-header custom-auth-form-header">{{ __('Reset Password') }}</div>
 
-                <div class="card-body">
+                <div class="card-body custom-auth-form-body">
                     <form method="POST" action="{{ route('password.update') }}">
                         @csrf
 
