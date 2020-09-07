88b35162da5de4e598abed4720c33cbc60bc86d6
Luca Santini <lucasantinidev@gmail.com>
2020-09-07 18:07:27 +0200

    pagina visualizzazioni

---
+++ resources/views/admin/statistics/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app_admin')
+
+@section('content')
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <h1>Statistiche appartamento</h1>
+                
+            </div>
+        </div>
+    </div>
+@endsection
