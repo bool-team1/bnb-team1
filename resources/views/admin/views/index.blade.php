fd1582113e454bf37d82242e5bc5b37a82897041
Luca Santini <lucasantinidev@gmail.com>
2020-09-09 14:15:01 +0200

    Input per selezionare appartamento per visualizzare statistiche

--- resources/views/admin/views/index.blade.php
+++ resources/views/admin/views/index.blade.php
@@ -6,6 +6,17 @@
             <div class="col-12">
                 <h1>Statistiche visualizzazioni appartamento</h1>
                 <div>
+                    <div class="input-group mb-3">
+                      <div class="input-group-prepend">
+                        <label class="input-group-text" for="inputGroupSelect01">Options</label>
+                      </div>
+                      <select class="custom-select" id="inputGroupSelect01">
+                        <option selected>Scegli</option>
+                        <option value="1">Jimmie Ranch</option>
+                        <option value="2">Clifton Rest</option>
+                        <option value="3">Everett Centers</option>
+                      </select>
+                    </div>
                     <canvas id="myChart"></canvas>
                 </div>
             </div>
