e000a9694216acbaa1d3e97da602c1b95b7fc162
George <quarelli.george@gmail.com>
2020-09-09 21:53:47 +0200

    Sidebar complete

--- resources/views/layouts/app_admin.blade.php
+++ resources/views/layouts/app_admin.blade.php
@@ -4,14 +4,15 @@
     <!-- menu + HEAD   -->
     @include("partials.menu")
     <!-- fine menu  -->
-  <label for="check">
-    <i class="fas fa-bars" id="sidebar_btn"></i>
-  </label>
+
   <div class="sidebar">
-    <center>
-      <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" alt="Profile Picture" class="profile_image">
-      <h4>USER NAME</h4>
-    </center>
+      <label for="check">
+        <i class="fas fa-bars" id="sidebar_btn"></i>
+      </label>
+    <!-- <center> -->
+      <!-- <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" alt="Profile Picture" class="profile_image"> -->
+      <!-- <h4>USER NAME</h4>
+    </center> -->
     <a href="{{route('admin.home')}}"><i class="far fa-building"></i><span>APPARTAMENTI</span></a>
     <a href="{{route('admin.views')}}"><i class="far fa-chart-bar"></i></i><span>STATISTICHE</span></a>
     <a href="{{route('admin.message.index')}}"><i class="far fa-envelope"></i></i><span>MESSAGGI</span></a>
@@ -20,8 +21,8 @@
   @yield('content')
 
   <!-- footer  -->
-   @include('partials.footer')
+  @include('partials.footer')
   <!-- fine footer  -->
-    @yield('script')
+  @yield('script')
 </body>
 </html>
