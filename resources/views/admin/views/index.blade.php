6b593429068df1e7ef3aeb0354379acb2928b84d
George <quarelli.george@gmail.com>
2020-09-18 02:22:24 +0200

    little_fix search view+ stat

--- resources/views/admin/views/index.blade.php
+++ resources/views/admin/views/index.blade.php
@@ -5,9 +5,10 @@
         var userID = "{{ Auth::user()->id }}";
     </script>
 @endif
+<main class="content col-lg-12 col-md-10 col-sm-4 new-apartament-cont">
     <div class="container">
-        <div class="row">
-            <div class="col-8 offset-4">
+        <div class="row fix-container new-apartament d-flex justify-content-center">
+            <div class="col-8">
                 <div class="input-group">
                     @if (count($apartments) <= 0)
                         @php
@@ -23,14 +24,14 @@
                     @endif
                 </div>
             </div>
-            <h2 id="view_apt_title" class="col-8 offset-4 text-center my-5"></h2>
-            <div class="col-8 offset-4">
+            <h2 id="view_apt_title" class="col-12 text-center my-5"></h2>
+            <div class="col-12">
                 <div id="msg_per_month">
                     <h2>Messaggi per mese</h2>
                     <canvas id="ChartMessage"></canvas>
                 </div>
             </div>
-            <div class="col-8 offset-4">
+            <div class="col-12">
                 <div id="views_per_month">
                     <h2>Visualizzazioni per mese</h2>
                     <canvas id="ChartViews"></canvas>
@@ -38,4 +39,5 @@
             </div>
         </div>
     </div>
+</main>
 @endsection
